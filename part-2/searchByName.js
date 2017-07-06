85d18f218bfcc48eadd36a2b7590d8a2fa197ce2
eobaah <eobaah@gmail.com>
2017-07-06 11:27:56 -0700

    add feedback from Trevor

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -1,21 +1,21 @@
 
-const fs = require('fs')
+const fs = require( 'fs' )
 
 let contacts = []
 
-fs.readFile('./clients.json', 'utf8', (err,data)=> {
+fs.readFile('./clients.json', 'utf8', ( err,data )=> {
   if(err) throw err;
 
-  let parsed = JSON.parse(data)
-  parsed.forEach( function(obj) {
+  let parsed = JSON.parse( data )
+  parsed.forEach( function( obj ) {
     let id = obj.id
     let rep_name = obj.rep_name
-    let args = process.argv[2]
-    if ( rep_name.toLowerCase().includes(args.toLowerCase() ) ) {
-      contacts.push({id,rep_name })
+    let searchTerm = process.argv[ 2 ]
+    if ( rep_name.toLowerCase().includes( searchTerm.toLowerCase() ) ) {
+      contacts.push( { id, rep_name } )
     } else {
       "Not found"
     }
   })
-  console.log(contacts)
+  console.log( contacts )
 })
--- part-2/searchByState.js
+++ part-2/searchByState.js
@@ -1,22 +1,22 @@
-const fs = require('fs')
+const fs = require( 'fs' )
 
 let states = []
 
-fs.readFile('./clients.json', (err, data) => {
-  if(err) throw err;
-  parsed = JSON.parse(data)
-  parsed.forEach( function(obj) {
+fs.readFile('./clients.json', ( err, data ) => {
+  if( err ) throw err;
+  parsed = JSON.parse( data )
+  parsed.forEach( function( obj ) {
     let id = obj.id
     let rep_name = obj.rep_name
     let company = obj.company
     let city = obj.city
     let state = obj.state
-    let arg = process.argv[2]
-    if( state.toLowerCase().includes(arg.toLowerCase() )) {
-      states.push( {id, rep_name, company, city, state})
+    let searchTerm = process.argv[ 2 ]
+    if( state.toLowerCase().includes( searchTerm.toLowerCase() ) ) {
+      states.push( { id, rep_name, company, city, state } )
     } else {
       return "Try again. Does not exist"
     }
-    console.log(states);
+    console.log( states );
   })
 })
