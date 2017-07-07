44f23c4694bb79b750b4e84bcfa27486eb15f5a7
eobaah <eobaah@gmail.com>
2017-07-07 10:02:20 -0700

    new changes from Trevor

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -6,10 +6,10 @@ let contacts = []
 fs.readFile('./clients.json', 'utf8', ( err,data )=> {
   if(err) throw err;
 
-  let parsed = JSON.parse( data )
+  const parsed = JSON.parse( data )
   parsed.forEach( function( obj ) {
-    let id = obj.id
-    let rep_name = obj.rep_name
+    const id = obj.id
+    const rep_name = obj.rep_name
     let searchTerm = process.argv[ 2 ]
     if ( rep_name.toLowerCase().includes( searchTerm.toLowerCase() ) ) {
       contacts.push( { id, rep_name } )
--- part-2/searchByState.js
+++ part-2/searchByState.js
@@ -6,11 +6,11 @@ fs.readFile('./clients.json', ( err, data ) => {
   if( err ) throw err;
   parsed = JSON.parse( data )
   parsed.forEach( function( obj ) {
-    let id = obj.id
-    let rep_name = obj.rep_name
-    let company = obj.company
-    let city = obj.city
-    let state = obj.state
+    const id = obj.id
+    const rep_name = obj.rep_name
+    const company = obj.company
+    const city = obj.city
+    const state = obj.state
     let searchTerm = process.argv[ 2 ]
     if( state.toLowerCase().includes( searchTerm.toLowerCase() ) ) {
       states.push( { id, rep_name, company, city, state } )
